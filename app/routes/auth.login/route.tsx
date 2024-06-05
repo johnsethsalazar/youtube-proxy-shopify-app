5a715b53f797a22f28e3ff2deb70c3a0bf88fd9a
johnsethsalazar <salazarjohnseth@gmail.com>
2024-06-05 15:37:45 +0800

    Added some function and graphql mutation in order to test the admin api and create a product

--- app/routes/auth.login/route.tsx
+++ app/routes/auth.login/route.tsx
@@ -1,20 +1,36 @@
-import {ActionFunction} from "@remix-run/node";
-import {authenticate} from "../../../app/shopify.server";
+import { ActionFunction, json } from "@remix-run/node";
+import { authenticate } from "../../../app/shopify.server";
 import { Page } from "@shopify/polaris";
 
 export const action: ActionFunction = async ({ request }) => {
-  console.log("----- Hit App Proxy---------")
+  console.log("----- Hit App Proxy---------");
 
-  const {session} = await authenticate.public.appProxy(request);
+  const { session, admin } = await authenticate.public.appProxy(request);
 
   if (session) {
-    console.log(session)
+    console.log(session);
   }
 
-  return null
-}
+  const response = await admin!.graphql(
+    `#graphql
+    mutation populateProduct($input: ProductInput!) {
+      productCreate(input: $input) {
+        product {
+          id
+        }
+      }
+    }`,
+    {variables: {input: {title: "YouTube Test Product"}}}
+  )
+
+  console.log(response, 'responseeeeee')
+
+  const productData = await response.json()
+
+  return json({data: productData.data})
+};
 
 const Proxy = () => {
-  return <Page>Proxy</Page>
-}
-export default Proxy
+  return <Page>Proxy</Page>;
+};
+export default Proxy;
